8739d103dfaa79d26891f80e60d2405bc8760ba9
Gergely Dremák <gergely089@gmail.com>
2021-03-09 15:38:40 +0100

    added 'if no message' to sentry error handler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,7 @@
 const Sentry = require("@sentry/node");
 
 module.exports = (err, req, res, next) => {
-	Sentry.captureException(err)
+	if(!err.message)
+		Sentry.captureException(err)
 	return res.send({ message: err.message })
 }
