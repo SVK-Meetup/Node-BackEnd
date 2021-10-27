af3d71d0dbee276a6c7c1222c7b2001aec8d1294
WingSMC <gergely089@gmail.com>
2021-10-27 21:04:15 +0200

    new default event color and date

--- src/config/default-event-options.js
+++ src/config/default-event-options.js
@@ -1,10 +1,14 @@
+const makeDateReadable = require("../util/makeDateReadable")
+
+const currentDate = new Date()
+
 module.exports = {
 	title: "Cím",
 	bannerURL: "https://res.cloudinary.com/wingsmc/image/upload/v1606431241/banner_f2hgpa.jpg",
 	shortdesc: "Rövid leírás/Alcím",
 	longdesc: "Product launch baseline shoot me an email yet locked and loaded, or to be inspired is to become creative, innovative and energized we want this philosophy to trickle down to all our stakeholders big data. That ipo will be a game-changer locked and loaded, and win-win. UX a loss a day will keep you focus form without content style without meaning we want to see more charts nor cross sabers to be inspired is to become creative, innovative and energized we want this philosophy to trickle down to all our stakeholders put in in a deck for our standup today. Going forward performance review nor we need to button up our approach so pig in a python. It just needs more cowbell digital literacy yet going forward good optics. Blue money criticality we need to harvest synergy effects. Guerrilla marketing. You can get my logo from facebook. Just do what you think. I trust you you can get my logo from facebook but you might wanna give it another shot, can you make it stand out more?, for can you please change the color theme of the website to pink and purple? make the logo a bit smaller because the logo is too big can you link the icons to my social media accounts? oh and please put pictures of cats everywhere. You might wanna give it another shot was i smoking crack when i sent this? hahaha! make the font bigger so is there a way we can make the page feel more introductory without being cheesy so labrador yet can you make it more infographic-y can you make it stand out more?. I have an awesome idea for a startup, i need you to build it for me try a more powerful colour, and make it sexy.",
-	date: new Date("2022-1-1 00:00"),
-	humanDate: "2021 január 1. 0:00",
+	date: currentDate,
+	humanDate: makeDateReadable(currentDate),
 	regActive: false,
 	presenters: [],
 	contacts: [],
@@ -23,7 +27,7 @@ module.exports = {
 		text1: "#ffffff",
 		text2: "#ffffff",
 		link: "#c44944",
-		highlight: "#ff2817",
+		highlight: "#e92b5a",
 		'moz-backfill': "#1c0135"
 	}
 }
